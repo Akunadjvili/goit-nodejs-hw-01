5e8b01d2f5235dae3f900de9010d6519e59af8a3
Volodymyr Davydenko <akunadjvili@gmail.com>
2021-08-03 21:23:26 +0300

    update: done homework

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# goit-nodejs-hw-01
- 
+```shell
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+node index.js --action list
+
+# Получаем контакт по id
+node index.js --action get --id 5
+
+# Добавялем контакт
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+# Удаляем контакт
+node index.js --action remove --id=3
+```
+
+| #   | action | screenshot                           |
+| --- | ------ | ------------------------------------ |
+| 1   | add    | [screenshot](https://ibb.co/RTSg8GT) |
+| 2   | get    | [screenshot](https://ibb.co/K5BJLWr) |
+| 2   | list   | [screenshot](https://ibb.co/VYFZbBR) |
+| 2   | remove | [screenshot](https://ibb.co/FnVxpJ8) |
---
+++ contacts.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const fs = require('fs');
+
+const contactsPath = path.join(__dirname, 'db/contacts.json');
+
+function listContacts() {
+  fs.readFile(contactsPath, 'utf8', function (error, data) {
+    if (error) {
+      throw error;
+    }
+    const contacts = JSON.parse(data);
+    console.table(contacts);
+  });
+}
+
+function getContactById(contactId) {
+  fs.readFile(contactsPath, 'utf8', function (error, data) {
+    if (error) {
+      throw error;
+    }
+    const contacts = JSON.parse(data);
+    const contact = contacts.find(({ id }) => id === Number(contactId));
+    console.table(contact);
+  });
+}
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath, 'utf8', function (error, data) {
+    if (error) {
+      throw error;
+    }
+    const contacts = JSON.parse(data);
+    const filteredContacts = contacts.filter(({ id }) => id !== Number(contactId));
+    const beautifyContacts = JSON.stringify(filteredContacts, null, 4);
+
+    fs.writeFile(contactsPath, beautifyContacts, function (error) {
+      if (error) {
+        throw error;
+      }
+      console.table(filteredContacts);
+    });
+  });
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath, 'utf8', function (error, data) {
+    if (error) {
+      throw error;
+    }
+    const contacts = JSON.parse(data);
+    const id = Math.max(...contacts.map(({ id }) => id)) + 1;
+    const contact = { id, name, email, phone };
+    const updatedContacts = [...contacts, contact];
+    const newContacts = JSON.stringify(updatedContacts, null, 4);
+    fs.writeFile(contactsPath, newContacts, function (error) {
+      if (error) {
+        throw error;
+      }
+      console.table(updatedContacts);
+    });
+  });
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
